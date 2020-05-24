f817c2de8414d520cf1b9932abf3aab9b43834d1
tomjn <contact@tomjn.com>
2020-05-24 14:02:48 +0100

    scaffold the single player screen and adjust the placeholder battles

--- src/js/components/Frame.js
+++ src/js/components/Frame.js
@@ -19,8 +19,6 @@ function Frame ( props ) {
 					<p className="menu-label">SinglePlayer</p>
 					<ul className="menu-list">
 						<li><Link to="/skirmish">Play Against AI</Link></li>
-						<li className="disabled"><a>Campaign</a></li>
-						<li className="disabled"><a>Replays</a></li>
 						<li><Link to="/settings">Settings</Link></li>
 					</ul>
 					<p className="menu-label">Current Battle</p>
@@ -31,17 +29,12 @@ function Frame ( props ) {
 					<ul className="menu-list">
 						<li><Link to="/battle/new">New Battle</Link></li>
 						<BattleNavItem name="Red Comet FFA" />
-						<BattleNavItem name="Red Comet FFA" />
-						<BattleNavItem name="Red Comet FFA" />
-						<BattleNavItem name="Red Comet FFA" />
-						<BattleNavItem name="Red Comet FFA" />
-						<BattleNavItem name="Red Comet FFA" />
-						<BattleNavItem name="2v2 Comet Catcher" />
-						<BattleNavItem name="2v2 Comet Catcher" />
-						<BattleNavItem name="2v2 Comet Catcher" />
-						<BattleNavItem name="2v2 Comet Catcher" />
-						<BattleNavItem name="2v2 Comet Catcher" />
-						<BattleNavItem name="2v2 Comet Catcher" />
+						<BattleNavItem name="Setons Clutch 1v1" />
+						<BattleNavItem name="Desert Triad 3v3" />
+						<BattleNavItem name="Comet Catcher 2v2" />
+						<BattleNavItem name="Delta Siege Dry 16v16" />
+						<BattleNavItem name="Speed Ball 1v1" />
+						<BattleNavItem name="Painted Desert 2v2" />
 					</ul>
 				</nav>
 			</div>
@@ -51,8 +44,130 @@ function Frame ( props ) {
 						<Route path="/skirmish">
 							<>
 								<h1 className="title">Play Against AI</h1>
-								<div className="box">
-									<p>Skirmish</p>
+								<div className="columns">
+									<div className="column is-two-thirds">
+										<div className="field has-addons">
+											<div className="field-label is-normal">
+												<label className="label">AIs</label>
+											</div>
+											<div className="field-body">
+												<p className="control">
+													<button className="button is-selected is-info">
+														<span>1</span>
+													</button>
+												</p>
+												<p className="control">
+													<button className="button">
+														<span>2</span>
+													</button>
+												</p>
+												<p className="control">
+													<button className="button">
+														<span>3</span>
+													</button>
+												</p>
+												<p className="control">
+													<button className="button">
+														<span>4</span>
+													</button>
+												</p>
+												<p className="control">
+													<button className="button">
+														<span>5</span>
+													</button>
+												</p>
+											</div>
+										</div>
+										<div className="field has-addons">
+											<div className="field-label is-normal">
+												<label className="label">Difficulty</label>
+											</div>
+											<div className="field-body">
+												<p className="control">
+													<button className="button">
+														<span>No AI</span>
+													</button>
+												</p>
+												<p className="control">
+													<button className="button is-selected is-info">
+														<span>Easy AI</span>
+													</button>
+												</p>
+												<p className="control">
+													<button className="button">
+														<span>Normal AI</span>
+													</button>
+												</p>
+												<p className="control">
+													<button className="button">
+														<span>Difficult AI</span>
+													</button>
+												</p>
+											</div>
+										</div>
+										<div className="field has-addons">
+											<div className="field-label is-normal">
+												<label className="label">Faction</label>
+											</div>
+											<div className="field-body">
+												<p className="control">
+													<button className="button is-selected is-info">
+														<span>Red</span>
+													</button>
+												</p>
+												<p className="control">
+													<button className="button">
+														<span>Blue</span>
+													</button>
+												</p>
+												<p className="control">
+													<button className="button">
+														<span>Scavengers</span>
+													</button>
+												</p>
+												<p className="control">
+													<button className="button">
+														<span>Random</span>
+													</button>
+												</p>
+											</div>
+										</div>
+										<div className="field has-addons">
+											<div className="field-label is-normal">
+												<label className="label">Map</label>
+											</div>
+											<div className="field-body">
+												<p className="control level">
+													<span class="level-left">
+														<span className="level-item">
+															Setons Clutch
+														</span>
+														<button className="button level-item">
+															<span>Change</span>
+														</button>
+													</span>
+												</p>
+											</div>
+										</div>
+										<hr />
+										<div className="field is-grouped">
+											<p className="control">
+												<button className="button is-primary">
+													<span>Start Game</span>
+												</button>
+											</p>
+											<p className="control">
+												<button className="button">
+													<span>Reset</span>
+												</button>
+											</p>
+										</div>
+									</div>
+									<div className="column is-one-third is-right">
+										<div className="box">
+											<p>Skirmish</p>
+										</div>
+									</div>
 								</div>
 							</>
 						</Route>
